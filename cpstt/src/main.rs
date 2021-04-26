993157a83e3cc3477afff12d42956d4ae3f05fcc
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-27 07:56:33 +0900

    feat: ファイル読み込みのエラーハンドリング追加

--- cpstt/src/main.rs
+++ cpstt/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::env;
+use anyhow::{Result};
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -13,19 +14,26 @@ use std::env;
 )]
 struct Opts {}
 
-fn main() {
+fn main() -> Result<()> {
     Opts::parse();
     let path = env::current_exe().unwrap();
-    print_logo(path);
+    print_logo(path)?;
+    Ok(())
 }
 
-fn print_logo(mut path: PathBuf) {
+/**
+ * CPSTTのロゴを出力
+ * @param path 実行形式ファイルへの絶対パス
+ * @return 正常終了の有無
+*/
+fn print_logo(mut path: PathBuf) -> Result<()> {
     for _i in 0..3 {
         path.pop();
     }
     path.push("logo.txt");
-    let f = File::open(path).unwrap();
-    for line in BufReader::new(f).lines() {
+    let file = File::open(path)?;
+    for line in BufReader::new(file).lines() {
         println!("{}", line.unwrap());
     }
+    Ok(())
 }
