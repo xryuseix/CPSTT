43cd53a7db9f63c35525641a273d97355ed94aba
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-27 05:26:31 +0900

    fix: ディレクトリ構成を変更

---
+++ cpstt/src/main.rs
@@ -0,0 +1,12 @@
+use clap::Clap;
+
+#[clap(
+    name = "CPSTT",
+    version = "1.0.0",
+    author = "xryuseix",
+    about = "Competitive Programming Stress Test Tools"
+)]
+
+fn main() {
+    println!("Hello, world!");
+}
