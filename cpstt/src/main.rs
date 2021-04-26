eb39f68ea205cf31b48db8e6a1c884f5fe0046a9
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-27 05:54:46 +0900

    feat: 実行時にロゴを出力

--- cpstt/src/main.rs
+++ cpstt/src/main.rs
@@ -1,12 +1,26 @@
 use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
+#[derive(Clap, Debug)]
 #[clap(
     name = "CPSTT",
     version = "1.0.0",
     author = "xryuseix",
     about = "Competitive Programming Stress Test Tools"
 )]
+struct Opts {}
 
 fn main() {
-    println!("Hello, world!");
+    Opts::parse();
+    print_logo();
+}
+
+fn print_logo() {
+    let f = File::open("./logo.txt").unwrap();
+    let reader = BufReader::new(f);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
