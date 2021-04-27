3c926f0f19de56badeeb4e07b9219e2b855d5158
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-28 03:01:54 +0900

    fix: ルートパスの所有権を渡さないようにした

--- cpstt/src/main.rs
+++ cpstt/src/main.rs
@@ -1,9 +1,10 @@
+use anyhow::Result;
 use clap::Clap;
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
-use std::env;
-use anyhow::{Result};
+// use std::process::Command;
 
 #[derive(Clap, Debug)]
 #[clap(
@@ -17,7 +18,10 @@ struct Opts {}
 fn main() -> Result<()> {
     Opts::parse();
     let path = env::current_exe().unwrap();
-    print_logo(path)?;
+    /* ロゴを出力 */
+    print_logo(path.clone())?;
+    /* generatorを実行 */
+    generator(path.clone())?;
     Ok(())
 }
 
@@ -25,8 +29,9 @@ fn main() -> Result<()> {
  * CPSTTのロゴを出力
  * @param path 実行形式ファイルへの絶対パス
  * @return 正常終了の有無
-*/
-fn print_logo(mut path: PathBuf) -> Result<()> {
+ */
+fn print_logo(root_path: PathBuf) -> Result<()> {
+    let mut path = root_path;
     for _i in 0..3 {
         path.pop();
     }
@@ -38,6 +43,35 @@ fn print_logo(mut path: PathBuf) -> Result<()> {
     Ok(())
 }
 
+/**
+ * generatorを実行
+ * @param path 実行形式ファイルへの絶対パス
+ * @return 正常終了の有無
+ */
+fn generator(mut path: PathBuf) -> Result<()> {
+    path.push("test/generator.cpp");
+    println!("{}", path.display());
+    // exec_cpp_program(path)?;
+    Ok(())
+}
+
+/*
+ * C++のファイルを指定し，そのプログラムを実行する
+ */
+// fn exec_cpp_program(_path: PathBuf) -> Result<()> {
+//     let mut output = Command::new("ls")
+//         .args(&["-l", "-a"])
+//         .spawn()
+//         .expect("failed to start `ls`");
+//     let stdout = output.stdout.take().unwrap();
+
+//     let reader = BufReader::new(stdout);
+//     for line in reader.lines() {
+//         println!("{}", line?);
+//     }
+//     Ok(())
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,13 +79,22 @@ mod tests {
     #[test]
     /**
      * ファイル読み込みテスト
-    */
+     */
     fn print_logo_test() {
-        let path = PathBuf::from(r"/Users/ryuse/Desktop/Algorithm Library/cpstt/cpstt/target/debug/cpstt");
+        let path =
+            PathBuf::from(r"/Users/ryuse/Desktop/Algorithm Library/cpstt/cpstt/target/debug/cpstt");
         let result_ok = print_logo(path);
         assert!(result_ok.is_ok());
         let path = PathBuf::from(r"/path/to");
         let result_ok = print_logo(path);
         assert!(result_ok.is_err());
     }
+
+    // #[test]
+    // fn exec_cpp_program_test() {
+    //     let path =
+    //         PathBuf::from(r"/Users/ryuse/Desktop/Algorithm Library/cpstt/cpstt/test/smart.cpp");
+    //     let result_ok = exec_cpp_program(path);
+    //     assert!(result_ok.is_ok());
+    // }
 }
