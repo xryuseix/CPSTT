7996cdd235c122c347921e147e8efffd47c0c0c2
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-29 21:19:38 +0900

    add smart c++ program

---
+++ test/smart.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello from CPP!" << endl;
+    int a, b;
+    cin >> a >> b;
+    cout << "OUTPUT: " << a << " " << b << endl;
+}
