18ba1bc9104f8d816902c1951c28bc0bb60828ce
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-30 08:12:40 +0900

    feat: read settings.toml

--- src/fileio.rs
+++ src/fileio.rs
@@ -1,4 +1,6 @@
 use anyhow::{bail, Result};
+use lazy_static::lazy_static;
+use serde::Deserialize;
 use std::env;
 use std::fs::{self, File};
 use std::io::Write;
@@ -7,6 +9,29 @@ use std::path::PathBuf;
 pub use crate::print_error::PrintError;
 
 pub struct MyFileIO {}
+
+#[derive(Debug, Deserialize)]
+pub struct SettingsClass {
+    pub execution: ExecutionSettings,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ExecutionSettings {
+    pub max_output_len: i32,
+}
+
+lazy_static! {
+    /**
+     * 設定ファイルの読み込み(実際にデータを持つのはこれ
+     * SETTING.execution.max_output_len のようにアクセスする
+     */
+    pub static ref SETTING: SettingsClass = {
+        let mut settings_path = MyFileIO::get_root_path().clone();
+        settings_path.push("settings.toml");
+        let settings = MyFileIO::read_settings(settings_path.clone()).unwrap();
+        settings
+    };
+}
 
 impl MyFileIO {
     /**
@@ -68,10 +93,33 @@ impl MyFileIO {
      * @return 異常終了: エラー
      *         正常終了: 実行結果の文字列
      */
-    pub fn file_write(path: &PathBuf, content: &String) -> Result<()> {
+    pub fn write_file(path: &PathBuf, content: &String) -> Result<()> {
         let mut file = File::create(path)?;
         write!(file, "{}", content)?;
         file.flush()?;
         Ok(())
+    }
+
+    /**
+     * ファイルの読み込み
+     * @param path ファイルへの絶対パス
+     * @return 異常終了: エラー
+     *         正常終了: 実行結果の文字列
+     */
+    pub fn read_file(path: String) -> Result<String, anyhow::Error> {
+        let file_content = fs::read_to_string(path)?;
+        Ok(file_content)
+    }
+
+    /**
+     * 設定ファイルの読み込み
+     * @param path settings.tomlへの絶対パス
+     * @return 異常終了: エラー
+     *         正常終了: 実行結果の文字列
+     */
+    pub fn read_settings(path: PathBuf) -> Result<SettingsClass, anyhow::Error> {
+        let settings_content = MyFileIO::read_file(String::from(path.to_str().unwrap()))?;
+        let settings: SettingsClass = toml::from_str(&settings_content).unwrap();
+        Ok(settings)
     }
 }
