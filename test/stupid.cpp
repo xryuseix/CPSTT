8614d96efd8fc7d87aa9f9a7d3c9cb8732471096
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-01 04:21:13 +0900

    test: TLEのケースを追加

--- test/stupid.cpp
+++ test/stupid.cpp
@@ -6,5 +6,13 @@ int main() {
     cout << "OUTPUT: " << a << " " << b << endl;
     if (a && b) {
         cout << string(50, 'A') << endl;
+        // int ans = 0;
+        // for (int i = 0; i < 100000; i++) {
+        //     for (int j = 0; j < 100000; j++) {
+        //         ans += (i * 12 + j) % 11;
+        //         ans %= 11;
+        //     }
+        // }
+        // cout << ans << endl;
     }
 }
