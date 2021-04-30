3a5c5012c716197003db1e69558f16cd074eeea0
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-01 01:40:09 +0900

    test: テストケースを更新

--- test/smart.cpp
+++ test/smart.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello from CPP!" << endl;
     int a, b;
     cin >> a >> b;
     cout << "OUTPUT: " << a << " " << b << endl;
-    if (a && b) {
-        cout << string(50, 'A') << endl;
-    }
 }
--- test/stupid.cpp
+++ test/stupid.cpp
@@ -3,10 +3,8 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << "I'm Stupid: " << a << " " << b << endl;
-    if (a && !b) {
-        for (int i = 0; i < 1000; i++) {
-            cout << char('A' + i % 26) << endl;
-        }
+    cout << "OUTPUT: " << a << " " << b << endl;
+    if (a && b) {
+        cout << string(50, 'A') << endl;
     }
 }
